5c834f9c5cb37055b11d4eea4b757f50a20a2042
Leg4tus <joonas.pirttikangas@gmail.com>
2025-12-05 22:20:44 +0200

    Initial commit

---
+++ HelloClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using System.Diagnostics;
+using HarmonyLib;
+using UnityEngine.UI;
+
+
+namespace HelloGladios
+{
+    public class HelloClass : IMod
+    {
+        public void Initialize()
+        {
+            var harmony = new Harmony("com.example.HelloGladio");
+            harmony.PatchAll();
+
+            UnityEngine.Debug.Log("Hello mod loaded");
+
+            DisplayMessage();
+
+
+            GeneralManager.singleton.gameObject.AddComponent<HelloComponent>();
+        }
+
+        public static void DisplayMessage()
+        {
+            GeneralManager.CreateConfirmDialog("This gets annoying fast", "Hello Gladios!", true);
+        }
+    }
+
+    [HarmonyPatch(typeof(GeneralManager), nameof(GeneralManager.CreateConfirmDialog))]
+    class Patch
+    {
+        static void Postfix(ref BasicConfirmDialog __result)
+        {
+            if (__result != null)
+            {
+                __result.UpdateText(__result.textField.text + "(patched hello)");
+            }
+            UnityEngine.Debug.Log("Harmony postfix!");
+        }
+    }
+}
---
+++ HelloComponent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace HelloGladios
+{
+    public class HelloComponent : MonoBehaviour
+    {
+       void Awake()
+        {
+            SceneManager.activeSceneChanged += SceneChanged;
+        }
+
+        private void SceneChanged(Scene arg0, Scene arg1)
+        {
+            HelloClass.DisplayMessage();
+        }
+
+        void OnDestroy()
+        {
+            SceneManager.activeSceneChanged -= SceneChanged;
+        }
+    }
+}
